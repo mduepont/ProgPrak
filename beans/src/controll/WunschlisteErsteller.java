117ce79149125b37c2d499ac0263f5245826dd55
hajiba <hbelmass@studi.hs-bremen.de>
2017-06-01 22:12:35 +0200

    add new file

---
+++ beans/src/controll/WunschlisteErsteller.java
@@ -0,0 +1,61 @@
+package controll;
+
+/**
+ * 
+ * @author Hajiba
+ *
+ */
+public class WunschlisteErsteller {
+	//----attribute
+	
+	 private String name;
+	 private int idWuenschListe;
+	 private String email;
+	 
+	 
+	public WunschlisteErsteller(String name, int idWuenschListe, String email) {
+		this.name = name;
+		this.idWuenschListe = idWuenschListe;
+		this.email = email;
+	}
+
+	public WunschlisteErsteller(){
+		super();
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getIdWuenschListe() {
+		return idWuenschListe;
+	}
+
+
+	public void setIdWuenschListe(int idWuenschListe) {
+		this.idWuenschListe = idWuenschListe;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	 
+	 
+	
+	 
+	 
+}
